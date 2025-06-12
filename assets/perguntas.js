fb166bb39a6c006c95a37bff3dc0c6ee5eb8e33d
elielciomartins <martins.elielcio@escola.pr.gov.br>
2025-06-12 08:00:46 -0300

    11346

---
+++ assets/perguntas.js
@@ -0,0 +1,17 @@
+criacartao(
+   'História'
+   'Em que ano terminou a segunda gurra mundial?'
+   '1945'
+
+criacartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
+
+criacartao(
+    categoria,
+    pergunta,
+    resposta
+)
