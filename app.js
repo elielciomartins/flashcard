d1a709b68648bffa388cb7d7818c4e6cb06563f4
elielciomartins <martins.elielcio@escola.pr.gov.br>
2025-06-26 08:24:32 -0300

    mdthjeuk

--- app.js
+++ app.js
@@ -15,9 +15,15 @@
     </div>
     `
 
-    let respostaestavisivel = false
+    let respostaEstaVisivel = false
 
-    
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+
+
     container.appendChild(cartao)
 
 }
